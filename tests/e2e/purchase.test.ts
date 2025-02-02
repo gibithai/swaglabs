1595d44cfbdac177dad41884952c5ceed902f5ef
gibithai <dougcarlin@tut.by>
2025-02-02 20:07:18 +0300

    Floki fixes for future interview

--- tests/e2e/purchase.test.ts
+++ tests/e2e/purchase.test.ts
@@ -1,32 +1,11 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/loginpage';
 
-test ('buy the goods', async ( {page}) =>{
+test('Login Test', async ({ page }) => {
+    const loginPage = new LoginPage(page);
     
-    await page.goto("https://www.saucedemo.com/")
+    await loginPage.goto();
+    await loginPage.login('standard_user', 'secret_sauce');
 
-    const pageTitle = page.title()
-    await expect(page).toHaveTitle("Swag Labs")
-
-    await page.locator('id=user-name').fill('standard_user')
-    await page.locator('id=password').fill('secret_sauce')
-
-    await page.locator('id=login-button').click()
-
-    await page.locator('id=add-to-cart-sauce-labs-backpack').click()
-    await page.locator('id=add-to-cart-sauce-labs-bolt-t-shirt').click()
-
-    await page.locator('.shopping_cart_link').click()
-    await page.locator('id=checkout').click()
-
-    // different method how we can call the selectors
-     await page.fill('id=first-name', 'Konstantin')
-     await page.fill('id=last-name', 'Ushak')
-     await page.fill('id=postal-code', '246000')
-
-     
-     await page.locator("id=continue").click()
-     await page.locator("id=finish").click()
-     await page.locator("id=back-to-products").click()
-
-     
-})
+    expect(await loginPage.getTitle()).toBe("Swag Labs");
+});
